90dc83ed129d22e5ac2942c91f7bfe71cc51315b
Ethan-Prieur1 <99287118+Ethan-Prieur1@users.noreply.github.com>
2022-11-10 10:04:21 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,29 @@
  */
 
 import promptSync from 'prompt-sync'
+/**
+*
+* @param {number} someInt parsedInt
+* @returns {number} factorial of number
+*/
 
-function factorial(integer: number): number {
+function factorial (integer: number): number {
   let answer
   if (integer < 0) {
     answer = -1
-  } else if ((integer = 0)) {
+  } else if (integer === 0) {
     answer = 1
   } else {
-    return factorial(integer - 1) * integer
+    answer = factorial(integer - 1) * integer
   }
   return answer
 }
 
+function main() {
 const prompt = promptSync()
 const userNumber = prompt('Enter a Number: ')
 const userInteger = parseInt(userNumber)
 console.log(`The Factorial of ${userInteger} is ${factorial(userInteger)}`)
 console.log('\nDone.')
+}
+main()
